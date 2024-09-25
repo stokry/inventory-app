7fbb70e9638212f1b78de2f794016d3981daf7a9
Mijo Kristo <45555307+stokry@users.noreply.github.com>
2024-09-25 10:09:58 +0000

    render

--- inventory_project/inventory_project/settings.py
+++ inventory_project/inventory_project/settings.py
@@ -32,7 +32,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
